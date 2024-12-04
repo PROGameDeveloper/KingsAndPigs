09f4886940e2f8a340995f868c2cb36a659a4b41
Mariano <marianitomotion@gmail.com>
2024-12-04 11:34:45 +0100

    Added Idle y Run states

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //COMPONENTS
     private Rigidbody2D m_rigidbody2D;
     private GatherInput m_gatherInput;
     private Transform m_transform;
+    private Animator m_animator;
+
+    //VALUES
     [SerializeField] private float speed;
     private int direction = 1;
+    private int IdSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,9 +21,20 @@ public class PlayerController : MonoBehaviour
         m_gatherInput = GetComponent<GatherInput>();
         m_transform = GetComponent<Transform>();
         m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
+        IdSpeed = Animator.StringToHash("Speed");
     }
 
-    // Update is called once per frame
+    private void Update()
+    {
+        SetAnimatorValues();
+    }
+
+    private void SetAnimatorValues()
+    {
+        m_animator.SetFloat(IdSpeed, Mathf.Abs(m_rigidbody2D.linearVelocityX));
+    }
+
     void FixedUpdate()
     {
         Move();
